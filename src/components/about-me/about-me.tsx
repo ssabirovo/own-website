2aef93045b8b16e5b192fb527fd63aff71a9728a
ssabirovo <muhiddinsobirov957@gmail.com>
2022-09-18 17:21:04 +0500

    first commit

---
+++ src/components/about-me/about-me.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./aboutMe.module.scss";
+
+interface AboutMeProps {}
+
+const AboutMe: React.FC<AboutMeProps> = () => (
+  <h1 className="">AboutMe Component</h1>
+);
+
+export default AboutMe;
