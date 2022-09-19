8003f51c5a8198cb0bf4a242cdc524b9ff98870c
Asilbek2002 <school2924@gmail.com>
2022-09-19 18:02:24 +0500

    finish projects component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { AboutMe, Technologies } from "./components";
 import "./assets/base.scss";
+import Projects from "./components/projects";
 
 function App() {
   return (
     <>
       <AboutMe />
       <Technologies />
+      <Projects />
     </>
   );
 }
